ebae3cd01d0bf0d4d2c6cb196e75f3f2f6072845
RyanShutUp <81351092+RyanShutUp@users.noreply.github.com>
2021-03-25 13:03:18 +0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+
+    console.log('i'm ready for duty!');
+
+});
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+        message.reply('pong')
+
+   };
+
+});
